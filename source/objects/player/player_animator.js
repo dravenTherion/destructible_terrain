43a82b78747002c0c8a947f510c28b0636a2ffa7
dravenTherion <i.am.quitoriano@gmail.com>
2020-02-26 07:35:32 +0800

    addedd comments for the player animator

--- source/objects/player/player_animator.js
+++ source/objects/player/player_animator.js
@@ -13,11 +13,16 @@ class PlayerAnimator extends Animator{
         this.bounceBody = [{val: 0}, {val: 0}, {val: 0}];
         this.bounceAnimation = [];
         
-        
+        // create the bounce animations for the upper body for when the player is 
+        // standing still or running
         this.bounceAnimation.push(new TimelineMax({paused: true}).to(this.bounceBody[0], 1, {val: 5, ease: Sine.easeInOut, repeat: 1, yoyo: true}));
         this.bounceAnimation.push(new TimelineMax({paused: true}).to(this.bounceBody[1], 0.5, {val: -5, ease: Sine.easeInOut, repeat: 4, yoyo: true}));
         
+        this.xBounceAdjust = 0.75;
+        this.yBounceAdjust = 22;
         
+        
+        // add the sprite animations for the player parts
         this.addSprite('body', new Sprite(0, 0, 92, 138, 25, 0, 44, 78, true, false));
         this.addSprite('armBack', new Sprite(92, 0, 92, 138, 25, 0, 40, 53, true, false));
         this.addSprite('armFront', new Sprite(184, 0, 92, 138, 25, 0, 40, 53, true, false));
@@ -35,9 +40,6 @@ class PlayerAnimator extends Animator{
         this.xDrawAdjust = 5;
         this.yDrawAdjust = 0;
         
-        this.xBounceAdjust = 0.75;
-        this.yBounceAdjust = 22;
-        
     }
     
     
@@ -46,7 +48,10 @@ class PlayerAnimator extends Animator{
         if(this.attachment === undefined)
             return;
         
+        this.centerX = player.x + player.width * 0.5;
+        this.centerY = player.y + player.height * 0.5;
      
+        
         const source = this.source,
               context = this.context,
               player = this.attachment,
@@ -55,15 +60,14 @@ class PlayerAnimator extends Animator{
               body = parts.body,
               armBack = parts.armBack,
               armFront = parts.armFront,
+              
+              // select the leg sprite based on the player movement state
               legsState = ((player.xSpeed !== 0) && !player.airborne) && player.canMove ? 1 : (player.airborne) ? 2: 0,
               legs = parts.legs[legsState],
               angleLegs = player.direction * player.angleStand * 0.6,
               
+              
               flash = parts.flash;             
-        
-        
-        this.centerX = player.x + player.width * 0.5;
-        this.centerY = player.y + player.height * 0.5;
         
         
         if(!player.airborne)
@@ -84,13 +88,15 @@ class PlayerAnimator extends Animator{
               
               flashVectors = Geom.vector(player.armLength + 50, player.angleLook);
         
-        
-        
+                
         vectorsBodyBounce.y *= player.angleStand > 0 ? vectorYAdj : player.angleStand < 0 ? -vectorYAdj : 1;
         
         
+        // play the leg animation
         if(!player.airborne)
         {
+            // if the player is running towards the direction it is facing play the leg animation in its normal direction
+            // else reverse it
             if((direction > 0 && player.xSpeed > 0) || (direction < 0 && player.xSpeed < 0))
                 legs.reverse = false
             else
@@ -100,6 +106,7 @@ class PlayerAnimator extends Animator{
         }
         else
         {
+            // play the leg jump animation
             if(legs.paused && player.ySpeed <= 0)
             {
                 legs.reverse = false;
@@ -108,6 +115,8 @@ class PlayerAnimator extends Animator{
             }
             else
             {
+                // during the jump, while the player is rising play the animation in the normal direction
+                // while falling play it in reverse
                 if(player.ySpeed < 0)
                     legs.reverse = false;
                 else
@@ -118,6 +127,7 @@ class PlayerAnimator extends Animator{
         }
         
         
+        // play the shooting animation
         if(player.isShooting && player.canShoot && armFront.paused && body.paused && armBack.paused)
         {
             armBack.paused = 
@@ -125,7 +135,6 @@ class PlayerAnimator extends Animator{
             body.paused = 
             flash.paused = false;
             
-            //flash.framesCurr = 1;
             player.canShoot = false;
         }
         else if(!player.canShoot && armFront.paused && body.paused && armBack.paused)
@@ -133,8 +142,7 @@ class PlayerAnimator extends Animator{
             player.canShoot = true;
             player.isShooting = false;
         }
-        
-        
+                
         if(!body.paused && !armBack.paused && !armFront.paused)
         {
             body.step();
@@ -146,45 +154,55 @@ class PlayerAnimator extends Animator{
         
         context.save();
         
-        
+        // translate the animator to the current position of the player
         context.translate(x, y);
+        
+        // flip the animator to the current direction of the player
         context.scale(direction, 1);
         
+        // rotate and draw the player legs based on the angle of the terrain
         this.rotateSprite(angleLegs, legs.transformOffsetX, legs.transformOffsetY);
         context.drawImage(source, legs.frameOffsetX, legs.frameHeight * legs.framesCurr, legs.frameWidth, legs.frameHeight, 0, 0, legs.frameWidth, legs.frameHeight);
         this.rotateSprite(-angleLegs, legs.transformOffsetX, legs.transformOffsetY);
         
-        
+        // animate the upper body based on the bounce animation of the legs
         context.translate(vectorsBodyBounce.y, vectorsBodyBounce.x * vectorXAdj);
         
+        // rotate and draw the player arm (behind the body) based on the angle of the target
         this.rotateSprite(player.angleLook * 0.95, armBack.transformOffsetX, armBack.transformOffsetY);
         context.drawImage(source, armBack.frameOffsetX, armBack.frameHeight * armBack.framesCurr, armBack.frameWidth, armBack.frameHeight, 0, 0, armBack.frameWidth, armBack.frameHeight);
         this.rotateSprite(-player.angleLook * 0.95, armBack.transformOffsetX, armBack.transformOffsetY);
         
+        // revert the uppper body animation translation
         context.translate(-vectorsBodyBounce.y, -vectorsBodyBounce.x * vectorXAdj);
         
         
+        // animate the upper body based on the bounce animation of the legs
         context.translate(vectorsBodyBounce.y, vectorsBodyBounce.x);
         
+        // rotate and draw the player arm (behind the body) based on the angle of the target
         this.rotateSprite(player.angleLook * 0.25, body.transformOffsetX, body.transformOffsetY);
         context.drawImage(source, body.frameOffsetX, body.frameHeight * body.framesCurr, body.frameWidth, body.frameHeight, 0, 0, body.frameWidth, body.frameHeight);
         this.rotateSprite(-player.angleLook * 0.25, body.transformOffsetX, body.transformOffsetY);
         
+        // revert the uppper body animation translation
         context.translate(-vectorsBodyBounce.y, -vectorsBodyBounce.x);
         
         
+        // animate the upper body based on the bounce animation of the legs
         context.translate(vectorsBodyBounce.y, vectorsBodyBounce.x * vectorXAdj);
         
+        // rotate and draw the player arm (behind the body) based on the angle of the target
         this.rotateSprite(player.angleLook, armFront.transformOffsetX, armFront.transformOffsetY);
         context.drawImage(source, armFront.frameOffsetX, armFront.frameHeight * armFront.framesCurr, armFront.frameWidth, armFront.frameHeight, 0, 0, armFront.frameWidth, armFront.frameHeight);
         
         
         if(!flash.paused)
         {
+            // draw the muzzle flash of the gun
             context.translate(flash.transformOffsetX, flash.transformOffsetY);   
             context.drawImage(source, flash.frameOffsetX, flash.frameOffsetY + flash.frameHeight * flash.framesCurr, flash.frameWidth, flash.frameHeight, 0, 0, flash.frameWidth, flash.frameHeight);
         }
-    
         
         
         context.restore();
